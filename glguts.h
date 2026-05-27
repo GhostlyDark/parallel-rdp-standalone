14c60423f7808d198c6ed3eb1d286c3a65c28ebf
Sergey <satan.death@mail.ru>
2026-05-27 20:45:01 +0400

    Update glguts.h

--- glguts.h
+++ glguts.h
@@ -39,6 +39,7 @@ extern "C"
     extern int32_t window_fullscreen;
     extern int32_t window_widescreen;
     extern int32_t window_vsync;
+    extern bool vk_integer_scaling;
 
 #ifdef __cplusplus
 }
