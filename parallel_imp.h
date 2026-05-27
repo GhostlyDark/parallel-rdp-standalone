96540c83730088777a7804b7014a17936edb532e
Sergey <satan.death@mail.ru>
2026-05-27 20:43:15 +0400

    Update parallel_imp.h

--- parallel_imp.h
+++ parallel_imp.h
@@ -25,6 +25,7 @@ extern "C"
     extern bool vk_vi_aa, vk_vi_scale, vk_dither_filter;
     extern bool vk_interlacing;
     extern bool vk_synchronous;
+    extern bool vk_integer_scaling;
     extern bool skip_swap_clear;
 
     void vk_rasterize();
